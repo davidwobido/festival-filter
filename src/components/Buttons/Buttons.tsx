c097f1083097553969a007359d53e65ff48a0844
davidwobido <davidwobido@googlemail.com>
2021-11-24 08:17:10 +0100

    :lipstick: Style buttons

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -4,7 +4,9 @@ import "../../globals.css";
 function Buttons() {
   return (
     <div className={styles.wrapper}>
-      <button className={styles["button-default"]}>Enter</button>
+      <button className={styles["button-default"]}>Button</button>
+      <button className={styles["button-wide"]}>Button</button>
+      <button className={styles["button-small"]}>Button</button>
     </div>
   );
 }
