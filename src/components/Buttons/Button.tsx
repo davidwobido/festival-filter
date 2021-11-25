59d9c92203ceb303733c58a0020b5433607667bc
davidwobido <davidwobido@googlemail.com>
2021-11-25 19:06:10 +0100

    :bug: Add width to button to prevent stretch in grid

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -4,7 +4,6 @@ import "../../globals.css";
 type ButtonProps = {
   size: "normal" | "wide" | "small";
   text: string;
-  className: string;
 };
 
 function Button({ size, text }: ButtonProps): JSX.Element {
