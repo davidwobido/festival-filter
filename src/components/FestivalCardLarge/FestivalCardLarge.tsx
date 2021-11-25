33014f6ec0ad6ff3bd96e9acd1c165b2ee4234b1
davidwobido <davidwobido@googlemail.com>
2021-11-25 14:53:31 +0100

    :lipstick: Style Festival Card Large

--- src/components/FestivalCardLarge/FestivalCardLarge.tsx
+++ src/components/FestivalCardLarge/FestivalCardLarge.tsx
@@ -3,36 +3,35 @@ import styles from "./FestivalCardLarge.module.css";
 
 function FestivalCardLarge() {
   return (
-    <div className={styles.wrapper}>
-      <h3>Melt!</h3>
-      <section className={styles.list}>
-        <div className={styles.row}>
-          <span>Location</span>
-          <span>Value</span>
+    <div className={styles.container}>
+      <div className={styles.wrapper}>
+        <div className={styles.header}>
+          <h3 className={styles.title}>Melt!</h3>
+          <img src="../../../lib/Icon_Close.svg" className={styles.close} />
         </div>
-        <div className={styles.row}>
-          <span>Date</span>
-          <span>Value</span>
-        </div>
-        <div className={styles.row}>
-          <span>Visitors</span>
-          <span>Value</span>
-        </div>
-        <div className={styles.row}>
-          <span>Acts</span>
-          <span>Value</span>
-        </div>
-        <div className={styles.row}>
-          <span>Price</span>
-          <span>Value</span>
-        </div>
-        <div className={styles.row}>
-          <span>URL</span>
-        </div>
-      </section>
-      <h4>Acts</h4>
-      <p>Alle the acts</p>
-      <Button size="small" text="Back to top" />
+        <section className={styles.list}>
+          <div className={styles.column}>
+            <span className={styles.item}>Location</span>
+            <span className={styles.item}>Date</span>
+            <span className={styles.item}>Visitors</span>
+            <span className={styles.item}>Acts</span>
+            <span className={styles.item}>Price</span>
+            <span className={styles.url}>Link to official website</span>
+          </div>
+          <div className={styles.column}>
+            <span className={styles.item}>Value</span>
+            <span className={styles.item}>Value</span>
+            <span className={styles.item}>Value</span>
+            <span className={styles.item}>Value</span>
+            <span className={styles.item}>Value</span>
+          </div>
+        </section>
+        <section className={styles.acts}>
+          <h4>Acts</h4>
+          <p>Alle the acts</p>
+        </section>
+        <Button size="small" text="Back to top" />
+      </div>
     </div>
   );
 }
