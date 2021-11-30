120924dd19240d79e21833bf41564c651fe7f053
davidwobido <davidwobido@googlemail.com>
2021-11-30 11:29:52 +0100

    :lipstick: Style page

--- src/pages/NameInput/NameInput.tsx
+++ src/pages/NameInput/NameInput.tsx
@@ -4,12 +4,18 @@ import styles from "./NameInput.module.css";
 
 function NameInput() {
   return (
-    <div className={styles.wrapper}>
-      <span className={styles.preline}>Who are you?</span>
-      <h2>Please tell us your name:</h2>
-      <AddUser />
-      <a className="skip">Show all festivals</a>
-    </div>
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <span className={styles.preline}>Who are you?</span>
+          <h2>Please tell us your name:</h2>
+          <AddUser />
+        </div>
+        <footer className={styles.footer}>
+          <a className={styles.skip}>Show all festivals</a>
+        </footer>
+      </div>
+    </>
   );
 }
 export default NameInput;
