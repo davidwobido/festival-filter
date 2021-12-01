da34cf8abbe1831d115c8c990d9fcdb91631b80e
davidwobido <davidwobido@googlemail.com>
2021-12-01 18:34:26 +0100

    :lipstick: Outlet is centered and navbar placeholder fixed

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,12 +1,11 @@
 import styles from "./NavBar.module.css";
-// import FF_Logo_small from "../../../lib/FF_Logo_small.svg";
 
 function NavBar() {
   return (
     <div className={styles.wrapper}>
       <a>Back</a>
       <img className={styles.logo} src="../../../lib/FF_Logo_small.svg" />
-      <div>Burger</div>
+      <p>Placeholder</p>
     </div>
   );
 }
