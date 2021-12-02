c7ca0b2921a69f67581d0ac99971bd668b81da33
davidwobido <davidwobido@googlemail.com>
2021-12-02 07:26:05 +0100

    :art: Delete unnecessary code and order css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import styles from "./App.module.css";
 import { Outlet } from "react-router";
 import NavBar from "./components/NavBar/NavBar";
-// import Loading from "./pages/Loading/Loading";
 
 function App() {
   return (
