af991547bc1f92bebbbe786c5703fa3ebffd3925
davidwobido <davidwobido@googlemail.com>
2021-12-03 14:39:41 +0100

    :sparkles: Add states to tag

--- src/components/GenreTag/GenreTag.tsx
+++ src/components/GenreTag/GenreTag.tsx
@@ -1,12 +1,23 @@
 import styles from "./GenreTag.module.css";
 import "../../globals.css";
+import { useState } from "react";
 
 type GenreTagProps = {
   text: string;
 };
 
 function GenreTag({ text }: GenreTagProps): JSX.Element {
-  return <button className={styles.tag}>{text}</button>;
+  const [active, setActive] = useState<boolean>(false);
+
+  function handleClick() {
+    active ? setActive(false) : setActive(true);
+  }
+
+  return (
+    <button className={styles.tag} onClick={handleClick}>
+      {text}
+    </button>
+  );
 }
 
 export default GenreTag;
