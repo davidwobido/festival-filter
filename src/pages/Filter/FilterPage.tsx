cce0c44584bcbb41b289771483f39d11ba30b318
davidwobido <davidwobido@googlemail.com>
2021-12-06 20:55:56 +0100

    :art: Outsource initialTags

--- src/pages/Filter/FilterPage.tsx
+++ src/pages/Filter/FilterPage.tsx
@@ -3,21 +3,23 @@ import GenreTag from "../../components/GenreTag/GenreTag";
 import Button from "../../components/Buttons/Button";
 import { useState } from "react";
 
-function SelectGenre() {
-  const [tags, setTags] = useState([
-    { text: "Pop", selected: false, id: 0 },
-    { text: "HipHop", selected: false, id: 1 },
-    { text: "Rock", selected: false, id: 2 },
-    { text: "Indie", selected: false, id: 3 },
-    { text: "Punk", selected: false, id: 4 },
-    { text: "Metal", selected: false, id: 5 },
-    { text: "Electronic", selected: false, id: 6 },
-    { text: "Reggae", selected: false, id: 7 },
-    { text: "Jazz", selected: false, id: 8 },
-    { text: "Classical", selected: false, id: 9 },
-  ]);
+const initialTags = [
+  { text: "Pop", selected: false, id: 0 },
+  { text: "HipHop", selected: false, id: 1 },
+  { text: "Rock", selected: false, id: 2 },
+  { text: "Indie", selected: false, id: 3 },
+  { text: "Punk", selected: false, id: 4 },
+  { text: "Metal", selected: false, id: 5 },
+  { text: "Electronic", selected: false, id: 6 },
+  { text: "Reggae", selected: false, id: 7 },
+  { text: "Jazz", selected: false, id: 8 },
+  { text: "Classical", selected: false, id: 9 },
+];
 
-  function onTagClicked(id: number) {
+function SelectGenre(): JSX.Element {
+  const [tags, setTags] = useState(initialTags);
+
+  function onTagClicked(id: number): void {
     const newTags = [...tags];
     const tag = newTags.find((tag) => tag.id === id);
     if (tag) {
@@ -27,25 +29,28 @@ function SelectGenre() {
   }
 
   return (
-    <>
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <span className={styles.preline}>
-            Hi {localStorage.getItem("ActiveUser")}
-          </span>
-          <h2>Choose your favorite genres:</h2>
-          <section className={styles.tags}>
-            {tags.map((tag) => (
-              <GenreTag tag={tag} key={tag.id} handleClick={onTagClicked} />
-            ))}
-          </section>
-        </div>
-        <footer className={styles.footer}>
-          <a className={styles.skip}>Show all festivals</a>
-        </footer>
-        <Button size="normal" text="filter" />
+    <div className={styles.wrapper}>
+      <div className={styles.content}>
+        <span className={styles.preline}>
+          Hi {localStorage.getItem("ActiveUser")}
+        </span>
+        <h2>Choose your favorite genres:</h2>
+        <section className={styles.tags}>
+          {tags.map((tag) => (
+            <GenreTag
+              // text={tag.text} selected={tag.selected} instead of tag={tag} does not work
+              tag={tag}
+              key={tag.id}
+              onClick={onTagClicked}
+            />
+          ))}
+        </section>
       </div>
-    </>
+      <footer className={styles.footer}>
+        <a className={styles.skip}>Show all festivals</a>
+      </footer>
+      <Button size="normal" text="filter" />
+    </div>
   );
 }
 export default SelectGenre;
