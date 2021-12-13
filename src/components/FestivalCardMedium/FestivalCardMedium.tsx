61db1cef4bb6d4145b9219c6ec9db3360cabbe6b
davidwobido <davidwobido@googlemail.com>
2021-12-13 20:10:18 +0100

    :sparkles: Make FestivalCardMedium dynamic to test filter

--- src/components/FestivalCardMedium/FestivalCardMedium.tsx
+++ src/components/FestivalCardMedium/FestivalCardMedium.tsx
@@ -1,25 +1,39 @@
 import Button from "../Buttons/Button";
 import styles from "./FestivalCardMedium.module.css";
+type FestivalCardMediumProps = {
+  name: string;
+  location: string;
+  begin: string;
+  end: string;
+  price: number;
+  allacts: string;
+};
 
-function FestivalCardMedium() {
+function FestivalCardMedium({
+  name,
+  location,
+  begin,
+  end,
+  allacts,
+  price,
+}: FestivalCardMediumProps) {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <span className={styles.match}>100%</span>
-        <h4 className={styles.name}>Festival</h4>
+        <h4 className={styles.name}>{name}</h4>
         <div className={styles["location-wrapper"]}>
           <img src="../../../lib/Icon_Location.svg" />
-          <span className={styles.location}> Location</span>
+          <span className={styles.location}>{location}</span>
         </div>
         <div className={styles["date-wrapper"]}>
           <img src="../../../lib/Icon_Date.svg" />
-          <span className={styles.date}> Date</span>
+          <span className={styles.date}>
+            {begin}—{end}
+          </span>
         </div>
-        <span className={styles.artists}>
-          Artist 1, Artist 2, Artist 3, Artist 4, Artist 5, Artist 6, Artist 7,
-          Artist 8, Artist 9, Artist 10
-        </span>
-        <span className={styles.price}>€€</span>
+        <span className={styles.artists}>{allacts}</span>
+        <span className={styles.price}>{price} €</span>
         <div className={styles.button}>
           <Button size="small" text="More" />
         </div>
