99be9212113971d57798734b47f2bef5370564fe
davidwobido <davidwobido@googlemail.com>
2021-12-15 09:23:13 +0100

    :ambulance: Add FestivalCard L + S to resolve merge conflict

--- src/components/FestivalCardLarge/FestivalCardLarge.stories.tsx
+++ src/components/FestivalCardLarge/FestivalCardLarge.stories.tsx
@@ -5,4 +5,16 @@ export default {
   title: "Components/Festival Card Large",
 };
 
-export const Card = () => <FestivalCardLarge />;
+export const Card = () => (
+  <FestivalCardLarge
+    name="Summer Breeze"
+    location="Dinkelsbühl"
+    begin="17.08.2022"
+    end="20.08.2022"
+    visitors={45000}
+    acts={140}
+    price={170}
+    allacts="Acranius, Agrypnie, Alestorm, Amorphis, Angelus Apatrida, Any Given Day, Die Apokalyptischen Reiter, Arch Enemy, Avatarium, Beast In Black, Benediction, Benighted, Blind Guardian, Bloodywood, Born From Pain, Brainstorm, Brothers Of Metal, Bury Tomorrow, Caliban, Cannibal Corpse, Combichrist, Comeback Kid, Conjurer, Crisix, Cypecore, Cytotoxin, Dagoba, Dangerface, Dark Funeral, Dark Tranquillity, Death Angel, Debauchery, Distant, Djerv, Eisbrecher, Eisregen, Emil Bulls, Enforced, Ensiferum, Feuerschwanz, Fiddler’s Green, Finntroll, Ghostkid, Gutalax, Hämatom, Haggefugg, Hammer King, Hangman’s Chair, Hatebreed, Heaven Shall Burn, Humanity’s Last Breath, Hypocrisy, Igorrr, Infected Rain, Insomnium, J.B.O., Jinjer, Korpiklaani, Landmvrks, Lik, Lord Of The Lost, Lorna Shore, Lüt, Madball, Mass Hysteria, Mental Cruelty, Misery Index, Mission In Black, Mr. Hurley & Die Pulveraffen, Napalm Death, Necrotted, Omnium Gatherum, Orden Ogan, Parasite Inc., Primal Fear, Seasons In Black, Serenity, Skyeye, Slope, Static X, Thundermother, Vola, Vulture, Warkings, Der Weg einer Freiheit, Within Temptation"
+    website="www.summer-breeze.de"
+  />
+);
--- src/components/FestivalCardLarge/FestivalCardLarge.tsx
+++ src/components/FestivalCardLarge/FestivalCardLarge.tsx
@@ -1,14 +1,42 @@
-import Button from "../Buttons/Button";
 import styles from "./FestivalCardLarge.module.css";
 
-function FestivalCardLarge() {
+export type FestivalCardLargeProps = {
+  name: string;
+  location: string;
+  begin: string;
+  end: string;
+  visitors: number;
+  acts: number;
+  price: number;
+  allacts: string;
+  website: string;
+  close?: () => void;
+};
+
+function FestivalCardLarge({
+  name,
+  location,
+  begin,
+  end,
+  visitors,
+  acts,
+  price,
+  allacts,
+  website,
+  close,
+}: FestivalCardLargeProps): JSX.Element {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <section className={styles.header}>
-          <h3 className={styles.title}>Festival</h3>
-          <img src="../../../lib/Icon_Close.svg" className={styles.close} />
+          <h3 className={styles.title}>{name}</h3>
+          <img
+            src="../../../lib/Icon_Close.svg"
+            className={styles.close}
+            onClick={close}
+          />
         </section>
+
         <section className={styles.list}>
           <div className={styles.column}>
             <span className={styles.item}>Location</span>
@@ -16,23 +44,24 @@ function FestivalCardLarge() {
             <span className={styles.item}>Visitors</span>
             <span className={styles.item}>Acts</span>
             <span className={styles.item}>Price</span>
-            <a className={styles.url}>Official website</a>
+            <a href={website} className={styles.url}>
+              Website
+            </a>
           </div>
           <div className={styles.column}>
-            <span className={styles.item}>Value</span>
-            <span className={styles.item}>Value</span>
-            <span className={styles.item}>Value</span>
-            <span className={styles.item}>Value</span>
-            <span className={styles.item}>Value</span>
+            <span className={styles.item}>{location}</span>
+            <span className={styles.item}>
+              {begin} — {end}
+            </span>
+            <span className={styles.item}>{visitors}</span>
+            <span className={styles.item}>{price} €</span>
+            <span className={styles.item}>{acts}</span>
           </div>
         </section>
         <section className={styles.acts}>
           <h4>Acts</h4>
-          <p>All the acts</p>
+          <p>{allacts}</p>
         </section>
-        <div className={styles.button}>
-          <Button size="small" text="Back to top" />
-        </div>
       </div>
     </div>
   );
