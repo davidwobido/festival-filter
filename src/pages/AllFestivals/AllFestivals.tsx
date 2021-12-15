8d5083083b195067b3eaaf172a3123f48c445a96
davidwobido <davidwobido@googlemail.com>
2021-12-15 09:24:52 +0100

    :ambulance: Add AllFestivals to resolve merge conflict

--- src/pages/AllFestivals/AllFestivals.tsx
+++ src/pages/AllFestivals/AllFestivals.tsx
@@ -1,4 +1,7 @@
 import { useEffect, useState } from "react";
+import FestivalCardLarge, {
+  FestivalCardLargeProps,
+} from "../../components/FestivalCardLarge/FestivalCardLarge";
 import FestivalCardSmall, {
   FestivalCardSmallProps,
 } from "../../components/FestivalCardSmall/FestivalCardSmall";
@@ -8,6 +11,8 @@ import styles from "./AllFestivals.module.css";
 function AllFestivals() {
   const [festivals, setFestivals] = useState<FestivalCardSmallProps[]>([]);
   const [search, setSearch] = useState("");
+  const [festival, setFestival] = useState<FestivalCardLargeProps | "">("");
+  const [query, setQuery] = useState<string | null>(null);
 
   const searchFestivals = festivals?.filter((festival) =>
     festival.name.toLocaleLowerCase().includes(search.toLowerCase())
@@ -22,28 +27,71 @@ function AllFestivals() {
     getFestivals();
   }, []);
 
+  useEffect(() => {
+    async function clickedFestival() {
+      if (query != "") {
+        const response = await fetch(`/api/festivals/name/${query}`);
+        const body = await response.json();
+        setFestival(body);
+      }
+    }
+    clickedFestival();
+  }, [query]);
+
+  function close() {
+    setFestival("");
+  }
+
   return (
     <div className={styles.wrapper}>
-      <section className={styles.text}>
-        <h1>All Festivals</h1>
-        <span className={styles.intro}>
-          Here’s an overview of all festivals in our database sorted by name.
-        </span>
-      </section>
-      <SearchBar onSearch={setSearch} />
-      <section className={styles.list}>
-        {searchFestivals?.length === 0 && (
-          <span className={styles["no-documents"]}>No Docouments found</span>
-        )}
-        {searchFestivals?.map((festival) => (
-          // eslint-disable-next-line react/jsx-key
-          <FestivalCardSmall
+      {!festival && (
+        <>
+          <section className={styles.text}>
+            <h1>All Festivals</h1>
+
+            <span className={styles.intro}>
+              Here’s an overview of all festivals in our database sorted by
+              name.
+            </span>
+          </section>
+          <SearchBar onSearch={setSearch} />
+          <section className={styles.list}>
+            {searchFestivals?.length === 0 && (
+              <span className={styles["no-documents"]}>
+                No Docouments found
+              </span>
+            )}
+
+            {searchFestivals?.map((festival) => (
+              // eslint-disable-next-line react/jsx-key
+              <FestivalCardSmall
+                name={festival.name}
+                location={festival.location}
+                begin={festival.begin}
+                end={festival.end}
+                toSearch={setQuery}
+              />
+            ))}
+          </section>
+        </>
+      )}
+
+      <section>
+        {festival && (
+          <FestivalCardLarge
+            close={() => close()}
+            key=""
             name={festival.name}
             location={festival.location}
             begin={festival.begin}
             end={festival.end}
+            visitors={festival.visitors}
+            acts={festival.acts}
+            price={festival.price}
+            allacts={festival.allacts}
+            website={festival.website}
           />
-        ))}
+        )}
       </section>
     </div>
   );
