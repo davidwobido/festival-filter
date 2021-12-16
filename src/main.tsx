a3c3198c88cc95414b9ad6202ab9297d0e0383a5
davidwobido <davidwobido@googlemail.com>
2021-12-16 20:02:24 +0100

    :bug: Try first example again

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./globals.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import StartPage from "./pages/Start/Start";
+import NameInput from "./pages/NameInput/NameInput";
+import FilterPage from "./pages/Filter/FilterPage";
+import FilterResult from "./pages/FilterResult/FilterResult";
+import AllFestivals from "./pages/AllFestivals/AllFestivals";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<StartPage />} />
+          <Route path="/login" element={<NameInput />} />
+          <Route path="/filter" element={<FilterPage />} />
+          <Route path="/filtered" element={<FilterResult />} />
+          <Route path="/all-festivals" element={<AllFestivals />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   </React.StrictMode>,
   document.querySelector("#root")
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./globals.css";
+// import App from "./App";
+// import { BrowserRouter } from "react-router-dom";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>,
+//   document.querySelector("#root")
+// );
