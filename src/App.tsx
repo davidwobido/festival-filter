b2fd1b72112e7df67a9c5e988ca9923fd9b16c8b
davidwobido <davidwobido@googlemail.com>
2021-12-16 17:58:51 +0100

    :bug: Change main.tsx +  app.tsx to repair ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import "./globals.css";
 import styles from "./App.module.css";
-import { Outlet } from "react-router";
-import NavBar from "./components/NavBar/NavBar";
+import { Route, Routes } from "react-router-dom";
+import StartPage from "./pages/Start/Start";
+import NameInput from "./pages/NameInput/NameInput";
+import FilterPage from "./pages/Filter/FilterPage";
+import FilterResult from "./pages/FilterResult/FilterResult";
+import AllFestivals from "./pages/AllFestivals/AllFestivals";
 
 function App() {
   return (
     <div className={styles.app}>
-      <NavBar />
-      <Outlet />
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<StartPage />} />
+          <Route path="login" element={<NameInput />} />
+          <Route path="filter" element={<FilterPage />} />
+          <Route path="filtered" element={<FilterResult />} />
+          <Route path="all-festivals" element={<AllFestivals />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,25 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./globals.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import StartPage from "./pages/Start/Start";
-import NameInput from "./pages/NameInput/NameInput";
-import FilterPage from "./pages/Filter/FilterPage";
-import FilterResult from "./pages/FilterResult/FilterResult";
-import AllFestivals from "./pages/AllFestivals/AllFestivals";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route index element={<StartPage />} />
-          <Route path="login" element={<NameInput />} />
-          <Route path="filter" element={<FilterPage />} />
-          <Route path="filtered" element={<FilterResult />} />
-          <Route path="all-festivals" element={<AllFestivals />} />
-        </Route>
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.querySelector("#root")
