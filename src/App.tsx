51631b8d335119bb90e634ed1d3783fada0b7798
davidwobido <davidwobido@googlemail.com>
2021-12-16 20:18:50 +0100

    :ambulance: Fixed Problems.

--- src/App.tsx
+++ src/App.tsx
@@ -13,27 +13,3 @@ function App() {
 }
 
 export default App;
-// import "./globals.css";
-// import styles from "./App.module.css";
-// import { Route, Routes } from "react-router-dom";
-// import StartPage from "./pages/Start/Start";
-// import NameInput from "./pages/NameInput/NameInput";
-// import FilterPage from "./pages/Filter/FilterPage";
-// import FilterResult from "./pages/FilterResult/FilterResult";
-// import AllFestivals from "./pages/AllFestivals/AllFestivals";
-
-// function App() {
-//   return (
-//     <div className={styles.app}>
-//       <Routes>
-//         <Route path="/" element={<StartPage />} />
-//         <Route path="/login" element={<NameInput />} />
-//         <Route path="/filter" element={<FilterPage />} />
-//         <Route path="/filtered" element={<FilterResult />} />
-//         <Route path="/all-festivals" element={<AllFestivals />} />
-//       </Routes>
-//     </div>
-//   );
-// }
-
-// export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -25,18 +25,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.querySelector("#root")
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import "./globals.css";
-// import App from "./App";
-// import { BrowserRouter } from "react-router-dom";
-
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//       <App />
-//     </BrowserRouter>
-//   </React.StrictMode>,
-//   document.querySelector("#root")
-// );
