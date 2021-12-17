3772c0c6460132a0a62a0b530cd36dd0d2016097
davidwobido <davidwobido@googlemail.com>
2021-12-17 20:54:36 +0100

    :lipstick: Dropdown and darken works fine

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -7,30 +7,48 @@ import { Link } from "react-router-dom";
 function NavBar() {
   const [menu, setMenu] = useState(false);
 
+  function handleClick() {
+    setMenu(!menu);
+  }
+
   return (
     <nav className={styles.navigation}>
+      <div className={styles.navbarbackground}></div>
+      <div className={` ${menu ? styles.surface : ""} `} />
       <Link to="/filter" className={styles.back}>
         <img className={styles.backicon} src={Back} /> Back
       </Link>
       <img src={Logo} className={styles.logo} />
-      <ul className={`${styles.navlinks} ${menu ? styles.open : ""} `}>
+      <ul
+        className={`${styles.navlinks} ${menu ? "" : styles.open} `}
+        onClick={() => handleClick()}
+      >
         <li>
           <Link className={styles.link} to="/filter">
             Filter
           </Link>
         </li>
         <li>
-          <Link className={styles.link} to="/all-festivals">
+          <Link
+            className={styles.link}
+            to="/all-festivals"
+            onClick={() => handleClick()}
+          >
             All Festivals
           </Link>
         </li>
         <li>
-          <Link className={styles.link} to="/start">
+          <Link className={styles.link} to="/" onClick={() => handleClick()}>
             About
           </Link>
         </li>
+        <li>
+          <Link className={`${styles.link} ${styles.user}`} to="/login">
+            Change User
+          </Link>
+        </li>
       </ul>
-      <div className={styles.burger} onClick={() => setMenu(!menu)}>
+      <div className={styles.burger} onClick={() => handleClick()}>
         <div className={`${menu ? styles.lineone : ""} `}></div>
         <div className={`${menu ? styles.linetwo : ""} `}></div>
         <div className={`${menu ? styles.linethree : ""} `}></div>
