22998aabc742247e5470b657df5037b3282d57ed
davidwobido <davidwobido@googlemail.com>
2021-12-18 10:52:12 +0100

    :sparkles: Add navigation to form

--- src/pages/NameInput/NameInput.tsx
+++ src/pages/NameInput/NameInput.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import AddUser from "../../components/AddUser/AddUserForm";
 import styles from "./NameInput.module.css";
-import { Link } from "react-router-dom";
 
 function NameInput() {
   return (
@@ -10,7 +9,7 @@ function NameInput() {
         <div className={styles.content}>
           <span className={styles.preline}>Who are you?</span>
           <h2>Please tell us your name:</h2>
-          <AddUser />
+          <AddUser navigateTo="/filter" />
         </div>
       </div>
     </>
