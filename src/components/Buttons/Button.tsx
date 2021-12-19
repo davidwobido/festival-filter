93a8e8346083e3ee26e185c889e5062ef47225da
davidwobido <davidwobido@googlemail.com>
2021-12-19 13:24:24 +0100

    :bug: Button finaly works with onClick

--- src/components/Buttons/Button.tsx
+++ src/components/Buttons/Button.tsx
@@ -4,10 +4,15 @@ import "../../globals.css";
 type ButtonProps = {
   size: "normal" | "wide" | "small";
   text: string;
+  onClick?: () => void;
 };
 
-function Button({ size, text }: ButtonProps): JSX.Element {
-  return <button className={styles[size]}>{text}</button>;
+function Button({ size, text, ...props }: ButtonProps): JSX.Element {
+  return (
+    <button className={styles[size]} {...props}>
+      {text}
+    </button>
+  );
 }
 
 export default Button;
