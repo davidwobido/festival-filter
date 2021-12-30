394aa27fb3efd2ce91bae6855a554f294b120349
davidwobido <davidwobido@googlemail.com>
2021-12-30 15:31:55 +0100

    :bug: Add Div100vh to Login-page

--- src/pages/NameInput/NameInput.tsx
+++ src/pages/NameInput/NameInput.tsx
@@ -1,18 +1,18 @@
 import AddUser from "../../components/AddUser/AddUserForm";
 import styles from "./NameInput.module.css";
 import LogoLarge from "../../../lib/FF_Logo_large.svg";
+import Div100vh from "react-div-100vh";
 
 function NameInput() {
   return (
-    <div className={styles.wrapper}>
+    <Div100vh className={styles.wrapper}>
       <img src={LogoLarge} className={styles.logo} />
-
       <div className={styles.content}>
         <span className={styles.preline}>Hello</span>
         <h2 className={styles.headline}>Tell us your name</h2>
         <AddUser navigateTo="/filter" />
       </div>
-    </div>
+    </Div100vh>
   );
 }
 export default NameInput;
